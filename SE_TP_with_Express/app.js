1ad7cf183fe0aa8215049507915def72db3b2835
LimeTasteLife <41933259+LimeTasteLife@users.noreply.github.com>
2022-05-12 19:09:41 +0900

    SE_TP first commit (edited)

---
+++ SE_TP_with_Express/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+
+const { sequelize } = require("./models");
+const indexRouter = require("./routes");
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    console.log("Success to connect DB");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(
+    `${req.method} ${req.url} \n no response for routers`
+  );
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  // res.render('error');
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), " waiting..");
+});
